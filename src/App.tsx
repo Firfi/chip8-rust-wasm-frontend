5622ba04d52cc913e22011d9c95d0b57c0b45b3a
Igor Loskutov <igor.loskutoff@gmail.com>
2022-05-11 19:55:56 +0700

    keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -70,6 +70,30 @@ const translateKeys = {
   86: 0xf // V
 } as const;
 
+// circumvent the "move" semantic ("already moved" between await calls); we collect the key presses between cpu iterations
+const initKbBuffer = () => {
+  let buffer: [number, 'up' | 'down'][] = [];
+  const makeCb = (kind: 'up' | 'down') => (e: KeyboardEvent) => {
+    buffer.push([e.which || e.keyCode, kind]);
+  }
+  const keyDownCb = makeCb('down');
+  const keyUpCb = makeCb('up');
+  document.addEventListener('keydown', keyDownCb);
+  document.addEventListener('keyup', keyUpCb);
+  return {
+    drop: () => {
+      document.removeEventListener('keydown', keyDownCb);
+      document.removeEventListener('keyup', keyUpCb);
+      buffer = [];
+    },
+    flush: () => {
+      const r = [...buffer];
+      buffer = [];
+      return r;
+    }
+  }
+}
+
 function App() {
   const [romData, setRomData] = useState<Uint8Array | null>(null);
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
@@ -79,24 +103,33 @@ function App() {
     if (!canvas) return;
     let stop = false;
     cpuRef.current = initChip8(romData, canvas);
+    let kbBuffer = initKbBuffer();
     const run = async () => {
       while (cpuRef.current && !cpuRef.current?.is_done()) {
         if (stop) {
           cpuRef.current?.stop();
+          cpuRef.current = null;
           return;
         }
-        cpuRef.current = await cpuRef.current?.run() || null;
+        const buffer = kbBuffer.flush();
+        buffer.forEach(([code, kind]) => {
+          cpuRef.current![kind === 'up' ? 'key_up' : 'key_down'](code);
+        })
+        cpuRef.current = await cpuRef.current!.run() || null;
       }
     };
     run();
     return () => {
       stop = true;
+      kbBuffer.drop();
     }
   }, [romData, canvas]);
   return (
     <div className="App">
+      <h1>Chip8 wasm on Rust</h1>
       <div><RomSelector onChange={setRomData} /></div>
       <div><canvas width={640} height={320} ref={setCanvas} /></div>
+      <div>Controls: 1234qwerasdfzxcv and whatnot</div>
     </div>
   );
 }
